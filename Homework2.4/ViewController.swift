22776a2037ad3d44356a07ba8a5345ac139e215b
Andrei MARKOV <a981203@mail.ru>
2020-08-31 14:08:52 +0300

    Initial Commit

---
+++ Homework2.4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework2.4
+//
+//  Created by Andrey Markov on 2020-08-31.
+//  Copyright © 2020 Andrey Markov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
