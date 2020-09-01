429de3d82fdc8be1c8abad431e90ca93ddc408eb
Andrei MARKOV <a981203@mail.ru>
2020-09-01 16:08:27 +0300

    test

--- Homework2.4/ViewController.swift
+++ Homework2.4/ViewController.swift
@@ -13,20 +13,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var button: UIButton!
     @IBOutlet weak var login: UITextField!
     @IBOutlet weak var password: UITextField!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
     
-    @IBAction func buttonPressed() {
-        let validLogin:String = "user"
-        let validPassword:String = "password"
-        func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-            if  login.text == validLogin && password.text == validPassword{
-                       performSegue(withIdentifier: "loginSegue", sender: nil)   }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "loginSegue"{
+            guard let mainVC = segue.destination as? MainViewController else {return}
             
         }
     }
-    
 }
 
